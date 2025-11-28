db901ea106a910b486b814d542cff8b93cd573dc
S4muelAndrey <161077886+S4muelAndrey@users.noreply.github.com>
2025-11-28 13:21:01 -0300

    Rename Colaboradores class to Colaborador

--- SQEVA/models/colaboradores.py
+++ SQEVA/models/colaboradores.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr, StringConstraints
 from typing import Annotated
 
 
-class Colaboradores(BaseModel):
+class Colaborador(BaseModel):
     nome: Annotated[str, StringConstraints(min_length=3)]
     matricula: Annotated[str, StringConstraints(min_length=14, max_length=14)]
     email: EmailStr
