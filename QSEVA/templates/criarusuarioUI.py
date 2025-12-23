6db09af1ab1bf3231342d40b4f48335ea685d79f
S4muelAndrey <161077886+S4muelAndrey@users.noreply.github.com>
2025-12-23 11:12:22 -0300

    criar usuario e templates

---
+++ QSEVA/templates/criarusuarioUI.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+class CriarUsuarioUI():
+    def main():
+        st.header("Criar usuário")
+        st.text_input("nome")
+        st.text_input("email")
+        st.text_input("telefone")
+        st.text_input("senha", type="password")
+        st.write("Permissões:")
+
+        interessado = st.checkbox("Interessado")
+        funcionario = st.checkbox("Funcionário")
+        administrador = st.checkbox("Administrador")
+
+        permissoes = []
+        if interessado:
+            permissoes.append("Interessado")
+        if funcionario:
+            permissoes.append("Funcionário")
+        if administrador:
+            permissoes.append("Administrador")
+        st.button("Criar Usuário")
